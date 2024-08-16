a51dd2c113b78d849478700b0c5eee8b57fcbd16
Alvaro Navarrete <115111730+Alvaro-Navarrete@users.noreply.github.com>
2024-08-16 02:24:40 -0400

    summary

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     let minorBtn = document.getElementById('minor-btn');
     let majorBtn = document.getElementById('major-btn');
@@ -29,19 +29,89 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var count = parseFloat(document.getElementById('count').innerText)
     // counter = 0;
-    minorBtn.addEventListener('click', function(){
+    minorBtn.addEventListener('click', function () {
         count--
         document.getElementById('count').innerText = count;
         updateMinorBtnState()
         updateMajorBtnState()
     })
 
-    majorBtn.addEventListener('click', function(){
+    majorBtn.addEventListener('click', function () {
         count++
         document.getElementById('count').innerText = count;
         updateMinorBtnState()
         updateMajorBtnState()
     })
+
+
+    var mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    function large() {
+        window.addEventListener('scroll', function () {
+            var summary = document.getElementById('summary');
+            var articles = document.querySelectorAll('.article');
+
+            var firstArticle = articles[0]
+            var lastArticle = articles[articles.length - 1];
+
+            var firstArticleTop = firstArticle.getBoundingClientRect().top + window.scrollY;
+            var lastArticleBottom = lastArticle.getBoundingClientRect().bottom + window.scrollY;
+
+            var stopPosition = lastArticleBottom - summary.offsetHeight;
+            var offsetTop = 30;
+
+            if (window.scrollY >= stopPosition) {
+                summary.style.position = 'absolute';
+                summary.style.top = stopPosition + 'px';
+            } else if (window.scrollY >= firstArticleTop) {
+                summary.style.position = 'fixed';
+                summary.style.top = offsetTop + 'px';
+            } else {
+                summary.style.position = 'absolute';
+                summary.style.top = firstArticleTop + 'px';
+            }
+        });
+    }
+
+    function middle() {
+        window.addEventListener('scroll', function () {
+            var summary = document.getElementById('summary');
+            var articles = document.querySelectorAll('.article');
+
+            var lastArticle = articles[articles.length - 1];
+            var lastArticleRect = lastArticle.getBoundingClientRect()
+
+            var lastArticleBottom = lastArticleRect.bottom + window.scrollY;
+            var summaryHeight = summary.offsetHeight;
+
+            var stopPosition = lastArticleBottom + 50;
+
+            var windowBottom = window.scrollY + window.innerHeight;
+            
+
+            if (windowBottom >= stopPosition + summaryHeight) {
+                summary.style.position = 'absolute';
+                summary.style.top = stopPosition + 'px';
+                summary.style.bottom = 'auto';
+            } else {
+                summary.style.position = 'fixed';
+                summary.style.bottom = '0';
+                summary.style.top = '';
+            }
+        });
+    }
+
+    function handleScreenChange(e) {
+        if (e.matches) {
+            middle();
+        } else {
+            large();
+        }
+    }
+
+    handleScreenChange(mediaQuery);
+
+    mediaQuery.addEventListener('change', handleScreenChange);
 
     updateMinorBtnState()
     updateMajorBtnState()
